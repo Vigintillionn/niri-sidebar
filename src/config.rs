3c9a71b13c2509bef4db2b792daa3ec305f0777a
Damian D'Souza <dsouzadamian189@gmail.com>
2026-02-12 23:42:24 +0100

    feat: add window_rule field to config
    stores a vector of window rules

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Result};
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::PathBuf;
@@ -10,6 +11,8 @@ pub struct Config {
     pub geometry: Geometry,
     pub margins: Margins,
     pub interaction: Interaction,
+    #[serde(default)]
+    pub window_rule: Vec<WindowRule>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -29,6 +32,16 @@ pub struct Margins {
 pub struct Interaction {
     pub peek: i32,
     pub focus_peek: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WindowRule {
+    #[serde(default, with = "serde_regex")]
+    pub app_id: Option<Regex>,
+    #[serde(default, with = "serde_regex")]
+    pub title: Option<Regex>,
+    pub width: i32,
+    pub height: i32,
 }
 
 impl Default for Config {
